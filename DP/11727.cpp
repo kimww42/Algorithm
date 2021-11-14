c941189748ba302c487728b7fa7c1650240e14c9
Siwon <kimww42@gmail.com>
2021-11-14 23:46:14 +0900

    Create 11727.cpp

---
+++ DP/11727.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/11727
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	int dp[1001] = {};
+	
+	scanf("%d", &n);
+	
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for(int i=3; i<=n; i++) {
+		dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+	}
+	printf("%d", dp[n]);
+	return 0;
+}
