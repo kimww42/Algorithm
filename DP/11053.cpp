a9d46553bb96fffa7a1516963019fc1e9373d253
Siwon <kimww42@gmail.com>
2021-11-21 22:40:18 +0900

    Create 11053.cpp

---
+++ DP/11053.cpp
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/11053
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, ans=0;
+	int arr[1009];
+	int dp[1009];
+	scanf("%d", &n);
+
+	for(int j=0; j<n; j++)
+		scanf("%d", &arr[j]);
+
+	for(int j=0; j<n; j++) {
+		int tmp=1;
+		for(int k=j-1; k>=0; k--) {
+			if(arr[j] > arr[k] && tmp < dp[k] + 1)
+				tmp = dp[k]+1;
+		}
+		dp[j] = tmp;
+		tmp=1;
+	}
+	
+	for(int i=0; i<n; i++) {
+		if(ans < dp[i])
+			ans = dp[i];
+	}
+	printf("%d\n", ans);
+	
+	return 0;
+}
